cde0395737ea4e03ea5304381b60e52a92809c4c
akash.kaintura <akash.kaintura@srijan.net>
2022-11-07 15:52:31 +0530

    add: entity for the blog

--- src/blog/model/blog-entry.interface.ts
+++ src/blog/model/blog-entry.interface.ts
@@ -1,1 +1,16 @@
-export interface BlogEntry {}
+import { User } from 'src/user/models/user.interface';
+
+export interface BlogEntry {
+  id?: number;
+  title?: string;
+  slug?: string;
+  description?: string;
+  body?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+  likes?: number;
+  author?: User;
+  headerImage?: string;
+  publishedDate?: Date;
+  isPublished?: boolean;
+}
